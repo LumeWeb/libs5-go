d7082976516243215295d94f6ae5da8571b35d41
Derrick Hammer <derrick@derrickhammer.com>
2024-01-05 09:02:58 -0500

    fix: ContentType shouldnt be a pointer

--- metadata/web_app_reference.go
+++ metadata/web_app_reference.go
@@ -3,11 +3,11 @@ package metadata
 import "git.lumeweb.com/LumeWeb/libs5-go/encoding"
 
 type WebAppMetadataFileReference struct {
-	ContentType *string
+	ContentType string
 	Cid         *encoding.CID
 }
 
-func NewWebAppMetadataFileReference(cid *encoding.CID, contentType *string) *WebAppMetadataFileReference {
+func NewWebAppMetadataFileReference(cid *encoding.CID, contentType string) *WebAppMetadataFileReference {
 	return &WebAppMetadataFileReference{
 		Cid:         cid,
 		ContentType: contentType,
