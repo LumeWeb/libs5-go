99167b4cec0f5105c2f15851e54bb21812c77b7d
Derrick Hammer <derrick@derrickhammer.com>
2024-01-07 04:15:28 -0500

    fix: add missing services impl

---
+++ node/services.go
@@ -0,0 +1,19 @@
+package node
+
+import "git.lumeweb.com/LumeWeb/libs5-go/interfaces"
+
+var (
+	_ interfaces.Services = (*ServicesImpl)(nil)
+)
+
+type ServicesImpl struct {
+	p2p interfaces.P2PService
+}
+
+func NewServices(p2p interfaces.P2PService) *ServicesImpl {
+	return &ServicesImpl{p2p: p2p}
+}
+
+func (s *ServicesImpl) P2P() interfaces.P2PService {
+	return s.p2p
+}
