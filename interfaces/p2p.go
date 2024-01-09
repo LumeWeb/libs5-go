27cc49fb45b869c34bb3d701ac70dccf8d676a94
Derrick Hammer <derrick@derrickhammer.com>
2024-01-09 06:56:03 -0500

    refactor: ReadNodeScore does not need to be in the interface

--- interfaces/p2p.go
+++ interfaces/p2p.go
@@ -18,7 +18,6 @@ type P2PService interface {
 	ConnectToNode(connectionUris []*url.URL, retried bool) error
 	OnNewPeer(peer net.Peer, verifyId bool) error
 	OnNewPeerListen(peer net.Peer, verifyId bool)
-	ReadNodeScore(nodeId *encoding.NodeId) (NodeVotes, error)
 	GetNodeScore(nodeId *encoding.NodeId) (float64, error)
 	SortNodesByScore(nodes []*encoding.NodeId) ([]*encoding.NodeId, error)
 	SignMessageSimple(message []byte) ([]byte, error)
