da57bc1f42b224e8ac1a51a66a5d90c8c1ad09da
Derrick Hammer <derrick@derrickhammer.com>
2024-01-24 11:51:10 -0500

    fix: fix again the port of EncodeEndian

--- utils/endian.go
+++ utils/endian.go
@@ -3,7 +3,7 @@ package utils
 func EncodeEndian(value uint64, length int) []byte {
 	res := make([]byte, length)
 
-	for i := length - 1; i >= 0; i-- {
+	for i := 0; i < length; i++ {
 		res[i] = byte(value & 0xff)
 		value = value >> 8
 	}
